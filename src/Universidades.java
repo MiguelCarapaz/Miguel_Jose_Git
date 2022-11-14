6d2dd0bf7cfa5abff4bc7b94242ad811c89ff1bd
jose <jose.pinos@epn.edu.ec>
2022-11-14 11:20:07 -0500

    reseteo clase unis

--- src/Universidades.java
+++ src/Universidades.java
@@ -1,64 +1,17 @@
 public class Universidades {
     //ATRIBUTOS
-    String nombre,direccion;
-    int carreras, alumnos, bibliotecas;
+    String nombreUni;
+    String direccionUni;
+    int num_Estudiantes;
+    int n_Carreras;
 
-    double costo_Carrera;
-
-        public Universidades(){
-            nombre="EPN";
-            direccion="AVD.12 de obtubre";
-            carreras=5;
-            alumnos=1000;
-            bibliotecas=5;
-
-        }
-        //METODOS
-
-    public void Registrar_Alumno(){}
-    public void Eliminar_Alumno(){}
-
-    //GETTER AND SETTER
-
-    public String getNombre() {
-        return nombre;
+    public void Universidades(){
+        nombreUni = "EPN";
+        direccionUni = "Patria";
+        num_Estudiantes = 1264;
+        n_Carreras = 30;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public int getCarreras() {
-        return carreras;
-    }
-
-    public void setCarreras(int carreras) {
-        this.carreras = carreras;
-    }
-
-    public int getAlumnos() {
-        return alumnos;
-    }
-
-    public void setAlumnos(int alumnos) {
-        this.alumnos = alumnos;
-    }
-
-    public int getBibliotecas() {
-        return bibliotecas;
-    }
-
-    public void setBibliotecas(int bibliotecas) {
-        this.bibliotecas = bibliotecas;
-    }
 
 
 }
