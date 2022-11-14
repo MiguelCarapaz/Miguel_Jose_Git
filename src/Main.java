098063e8edb84b835a705416c0ed5331412c4258
jose <jose.pinos@epn.edu.ec>
2022-11-14 11:37:44 -0500

    kil mi

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) throws Exception {
 
             String prueba1="Miguel";
+            double dinero = Double.parseDouble(JOptionPane.showInputDialog("Ingresa dinero") );
+        System.out.println("tienes " + dinero + " dollars");
 
     }
 }
