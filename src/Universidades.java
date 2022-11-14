2d3d1f828919b1988af39aff612248a4ae292e17
jose <jose.pinos@epn.edu.ec>
2022-11-14 11:02:58 -0500

    aniadir edad

--- src/Universidades.java
+++ src/Universidades.java
@@ -3,5 +3,9 @@ public class Universidades{
     public static void main(String[] args) {
         String nombre_test = JOptionPane.showInputDialog("Ingresa tu nombre");
         System.out.println("Bienvenido " + nombre_test);
+
+        int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingresa tu edad"));
+
+        System.out.println(nombre_test + " tiene " + edad);
     }
 }
