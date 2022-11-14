b721823da1a3b76a64d7c2e507387b60d5386b91
jose <jose.pinos@epn.edu.ec>
2022-11-14 10:41:57 -0500

    Segundo commit con un hola mundo

--- src/Universidades.java
+++ src/Universidades.java
@@ -3,5 +3,7 @@ public class Universidades {
     String nombre,direccion;
     int carreras, alumnos, bibliotecas;
 
-
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
 }
